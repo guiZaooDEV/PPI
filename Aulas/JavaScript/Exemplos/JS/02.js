3942f59008bbc3cc7d924da978438e4844a73765
guilherme.tscha <guilherme.tscha2@gmail.com>
2025-03-25 05:34:17 -0300

    aula

--- Aulas/JavaScript/Exemplos/JS/02.js
+++
@@ -1,7 +0,0 @@
-let contador=0
-let button=document.querySelector("button");
-button.addEventListener("keypress",(evento)=>{
-    console.log(evento.log)
-    contador++;
-    button.innerHTML=contador+evento.key+" KeyCode"+evento.keycode
-})
